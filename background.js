d5f13e291e855ddb1a71d0d5b3bd4ae20465fe60
Andrzej <andrzmichal@gmail.com>
2019-05-05 22:19:57 +0200

    Simple example of working code

--- background.js
+++ background.js
@@ -1,12 +1,28 @@
-getCookies("https://odoo.servocode.com", "session_id", function(id) {
-    alert(id);
-    console.log(id);
-});
-
-function getCookies(domain, name, callback) {
-    chrome.cookies.get({"url": domain, "name": name}, function(cookie) {
-        if(callback) {
-            callback(cookie.value);
-        }
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // Send a message to the active tab
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
     });
-}
+  });
+
+// getCookies("https://odoo.servocode.com", "session_id", function(id) {
+//     alert(id);
+//     console.log(id);
+// });
+
+// function getCookies(domain, name, callback) {
+//     chrome.cookies.get({"url": domain, "name": name}, function(cookie) {
+//         if(callback) {
+//             callback(cookie.value);
+//         }
+//     });
+// }
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if( request.message === "open_new_tab" ) {
+        chrome.tabs.create({"url": request.url});
+      }
+    }
+  );
--- content.js
+++ content.js
@@ -1,1 +1,11 @@
-console.log("We fill attendances!!!");
+alert("Hello from extension which fill attendances!!!");
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if( request.message === "clicked_browser_action" ) {
+  
+        console.log("Clicked action");
+        chrome.runtime.sendMessage({"message": "open_new_tab", "url": "http://google.com"});
+      }
+    }
+  );
